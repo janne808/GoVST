7753f3e778109ea31f0c9f51906267f763346839
Janne Heikkarainen <janne808@radiofreerobotron.net>
2019-06-02 09:06:52 +0200

    Remove old junk

--- cwrapper.cpp
+++
@@ -1,8 +0,0 @@
-#include "again.h"
-#include "cwrapper.h"
-
-extern "C" {
-        AudioEffect* VSTClass_new(audioMasterCallback audioMaster) {
-                return new AGain(audioMaster);
-        }
-}
